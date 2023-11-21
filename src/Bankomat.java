0fb2b480f66565e73b41559ca8b8a3298df32afe
Yahor \|/ <yagorpraca@gmail.com>
2023-11-21 18:58:53 +0300

    готово

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -19,13 +19,29 @@ public class Bankomat {
          this.bablo = bablo;
     }
 
-    public boolean moneyOutB(int bill20, int bill50, int bill100) {
-        this.bill20 -= bill20;
-        this.bill50 -= bill50;
-        this.bill100 -= bill100;
-         int lave = (bill20 * 20) + (bill50 * 50) + (bill100 * 100);
-        if (this.bablo <= lave ) {
-return false;
-        }else return true;
+    public boolean moneyOutB(int lave) {
+        if (bablo < lave) {
+            return false;
+        }
+        int count100 = Math.min(lave / 100, this.bill100);
+        lave -= count100 * 100;
+        this.bill100 -= count100;
+
+        int count50 = Math.min(lave / 50, this.bill50);
+        lave -= count50 * 50;
+        this.bill50 -= count50;
+
+        int count20 = Math.min(lave / 20, this.bill20);
+        lave -= count20 * 20;
+        this.bill20 -= count20;
+
+        if (lave > 0) {
+            return false;
+        }
+
+        this.bablo -= lave;
+        System.out.println("Выдано купюр: " + count100 + " х 100, " + count50 + " х 50, " + count20 + " х 20");
+        return true;
+    }
     }
 }
