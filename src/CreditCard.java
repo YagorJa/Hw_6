139fda1b5edbde48d71090c4b38b3c03fe1bd358
Yahor \|/ <yagorpraca@gmail.com>
2023-11-21 16:10:14 +0300

    улучшенная логика и проверка счёта

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,22 +1,25 @@
 public class CreditCard {
     int cardNumber;
-    double moneyCheck ;
+    double moneyCheck;
 
     public CreditCard(int cardNumber, double moneyCheck) {
         this.cardNumber = cardNumber;
         this.moneyCheck = moneyCheck;
     }
- public void moneyAdd()
-    {
-    double moneyP = 2000;
-         moneyCheck += moneyP;
+
+    public void moneyAdd(double moneyP) {
+        moneyCheck += moneyP;
     }
-public void moneyOut() {
-        double moneyM = 600;
-        moneyCheck -= moneyM;
-}
-public void moneySout(){
-    System.out.println(moneyCheck);
-}
+
+    public void moneyOut(double moneyM) {
+        if (moneyM <= moneyCheck) {
+            moneyCheck -= moneyM;
+        } else System.out.println("НЕДОСТАТОЧНО средств на карте " + cardNumber + "\n");
+    }
+
+    public void moneySout() {
+        System.out.println("Номер банковской карты: " + cardNumber);
+        System.out.println("Текущий баланс: " + moneyCheck + "$");
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         CreditCard card1 = new CreditCard(7347474, 8000);
          CreditCard card2 = new  CreditCard(645644,6784.42);
          CreditCard card3 = new CreditCard(333335, 1983.54);
-         card1.moneyAdd();
-         card2.moneyAdd();
-         card3.moneyOut();
+         card1.moneyAdd(436.08);
+         card2.moneyAdd(1200);
+         card3.moneyOut(1985);
        card1.moneySout();
        card2.moneySout();
        card3.moneySout();
